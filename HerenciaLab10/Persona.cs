d48571cfd09af76f81bf4c8a6ba889e05884f515
oscar_rodriguez <oscar_rodriguez@LAB4-21>
2024-04-25 08:57:52 -0600

    Agregar archivos de proyecto.

---
+++ HerenciaLab10/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerenciaLab10
+{
+    public class Persona
+    {
+        public int Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento  { get; set; }
+
+        public string Edad { get; set; }
+
+        public Persona()
+        {
+            
+        }
+    }
+}
